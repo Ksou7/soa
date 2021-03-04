e700e13335e57d06ee639bde1350660bade6ac2b
Mohammed Amine Grid <65062069+mohammed-amine-grid@users.noreply.github.com>
2021-03-04 20:19:43 +0100

    proxy

--- server/router/bundles.js
+++ server/router/bundles.js
@@ -1,26 +1,10 @@
-const { Router } = require('express');
-const { createProxyMiddleware } = require('http-proxy-middleware');
-const {
-  service1, service2, service3, service4,
-} = require('../config/services.js');
+const { Router } = require("express");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+const { service4, service2, service1 , service3} = require("../config/services.js");
 
 const router = Router();
 
-router.use('/service1.js', createProxyMiddleware({
-  target: service1.url,
-  pathRewrite: {
-    '^/bundles/service1.js': service1.bundle,
-  },
-  changeOrigin: true,
-}));
 
-router.use('/service2.js', createProxyMiddleware({
-  target: service2.url,
-  pathRewrite: {
-    '^/bundles/service2.js': service2.bundle,
-  },
-  changeOrigin: true,
-}));
 
 router.use('/service3.js', createProxyMiddleware({
   target: service3.url,
@@ -30,12 +14,37 @@ router.use('/service3.js', createProxyMiddleware({
   changeOrigin: true,
 }));
 
-router.use('/service4.js', createProxyMiddleware({
-  target: service4.url,
-  pathRewrite: {
-    '^/bundles/service4.js': service4.bundle,
-  },
-  changeOrigin: true,
-}));
+// router.use(
+//   "/service1.js",
+//   createProxyMiddleware({
+//     target: service1.url,
+//     pathRewrite: {
+//       "^/bundles/service1.js": service1.bundle,
+//     },
+//     changeOrigin: true,
+//   })
+// );
+
+// router.use(
+//   "/service4.js",
+//   createProxyMiddleware({
+//     target: service4.url,
+//     pathRewrite: {
+//       "^/bundles/service4.js": service4.bundle,
+//     },
+//     changeOrigin: true,
+//   })
+// );
+
+// router.use(
+//   "/service2.js",
+//   createProxyMiddleware({
+//     target: service2.url,
+//     pathRewrite: {
+//       "^/bundles/service2.js": service2.bundle,
+//     },
+//     changeOrigin: true,
+//   })
+// );
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,17 +1,53 @@
-const express = require('express');
-const morgan = require('morgan');
-const path = require('path');
-const router = require('./router');
+const express = require("express");
+const morgan = require("morgan");
+const path = require("path");
+const router = require("./router");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+var cors = require("cors");
 
-const PUBLIC_DIR = path.resolve(__dirname, '..', 'public');
+const PUBLIC_DIR = path.resolve(__dirname, "..", "public");
 const app = express();
 
-app.use(morgan('dev'));
+app.use(cors());
+app.use(morgan("dev"));
 app.use(express.static(PUBLIC_DIR));
 
 // Handling asset requests for webpack bundles by passing off requests to the bundles router
-app.use('/bundles', router.bundles);
+app.use("/bundles", router.bundles);
 // Handling AJAX requests to the API by passing off requests to the api router
-app.use('/api', router.api);
+// app.use("/api", router.api);
+
+// app.use(
+//   "/api/reviews",
+//   createProxyMiddleware({
+//     target: "http://localhost:3004",
+//     changeOrigin: true,
+//     // pathRewrite: {
+//     //   "^/api/reviews": "",
+//     // },
+//   })
+// );
+
+// app.use(
+//   "/api/questions",
+//   createProxyMiddleware({
+//     target: "http://localhost:3002",
+//     changeOrigin: true,
+//   })
+// );
+// app.use(
+//   "/api/products/:product_id",
+//   createProxyMiddleware({
+//     target: "http://localhost:3001",
+//     changeOrigin: true,
+//   })
+// );
+app.use(
+  "/api/overview",
+  createProxyMiddleware({
+    target: "http://localhost:3003",
+    changeOrigin: true,
+  })
+);
 
 module.exports = app;
