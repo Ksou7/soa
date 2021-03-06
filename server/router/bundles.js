bb678884c3791d6a3419184ffdf6fb8128412bde
Mohammed Amine Grid <65062069+mohammed-amine-grid@users.noreply.github.com>
2021-03-06 13:43:31 +0100

    fixing style bug

--- server/router/bundles.js
+++ server/router/bundles.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 const {
   service4,
   service2,
-  service1,
+  // service1,
   service3,
 } = require("../config/services.js");
 
